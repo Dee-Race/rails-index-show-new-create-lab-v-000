ebb0ea26d6a5afe432e7c12150b1f94604af8ab7
Dee Race <deerace12@gmail.com>
2018-06-12 21:21:00 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,14 +1,14 @@
 class CouponsController < ApplicationController
 
   def index
-    @coupons = Coupon.all 
+    @coupons = Coupon.all
   end
 
   def show
     @coupon = Coupon.find(params[:id])
   end
 
-  def new 
+  def new
     @coupon = Coupon.new
   end
 
